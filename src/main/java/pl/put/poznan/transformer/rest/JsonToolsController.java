02614b85ad011e4b145e38cfb302f183553095e0
Mikołaj Walkowski <mikolaj.walkowski.00@gmail.com>
2021-12-06 17:42:03 +0100

    REST dla oneline i filterOut

--- src/main/java/pl/put/poznan/transformer/rest/JsonToolsController.java
+++ src/main/java/pl/put/poznan/transformer/rest/JsonToolsController.java
@@ -2,44 +2,38 @@ package pl.put.poznan.transformer.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.transformer.logic.TextTransformer;
+import pl.put.poznan.transformer.logic.JsonInterpreter;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 
 @RestController
-@RequestMapping("/{text}")
-public class TextTransformerController {
+public class JsonToolsController {
 
-    private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
-
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
-
+    //private static final Logger logger = LoggerFactory.getLogger(JsonToolsController.class);
+    @GetMapping("/filterOut/{text}")
+    public String filterOut(@PathVariable String text,@RequestParam(value="filters", defaultValue="") String[] filters, @RequestParam(value="format", defaultValue="beautiful") String format) {
         // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // perform the transformation, you should run your logic here, below is just a silly example
-        TextTransformer transformer = new TextTransformer(transforms);
-        return transformer.transform(text);
+        //logger.debug(text);
+        //logger.debug(Arrays.toString(transforms));
+        JsonInterpreter out;
+        if(Objects.equals(format, "beautiful")){
+            out = new JsonBeautiful(text);
+        }else if(Objects.equals(format, "oneLine")){
+            out = new JsonOneLine(text);
+        }
+        out = new JsonFilterOut(out,filters);
+        return out.show();
     }
-
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
-
+    @GetMapping("/oneLine/{text}")
+    public String filterOut(@PathVariable String text) {
         // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // perform the transformation, you should run your logic here, below is just a silly example
-        TextTransformer transformer = new TextTransformer(transforms);
-        return "aaaaa";
+        //logger.debug(text);
+        //logger.debug(Arrays.toString(transforms));
+        JsonInterpreter out = new JsonOneLine(text);
+        return out.show();
     }
-
-
 
 }
 
