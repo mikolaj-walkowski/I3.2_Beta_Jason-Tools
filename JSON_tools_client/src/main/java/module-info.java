7a78709b5486dfb654e9aba7dc90829fc8b5ae2b
Mikołaj <mikolaj.walkowski.00@gmail.com>
2022-01-21 14:48:43 +0100

    Javadoc fun v2

--- JSON_tools_client/src/main/java/module-info.java
+++ JSON_tools_client/src/main/java/module-info.java
@@ -1,8 +1,4 @@
-module com.example.json_tools_client {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.json_tools_client to javafx.fxml;
-    exports com.example.json_tools_client;
-}
+/**
+ * Main package
+ * 
+ */
