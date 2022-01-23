b3141eac01ceace1eaba804e361beaa788a0bee6
Witold <wandraszyk73@gmail.com>
2022-01-23 16:11:33 +0100

    FilterOutTest

---
+++ RestApi/src/main/java/pl/put/poznan/tools/logic/JsonFilterOutTest.java
@@ -0,0 +1,12 @@
+package pl.put.poznan.tools.logic;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonFilterOutTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
