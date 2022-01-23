59f26b7ddd6ecfa6db57802af0961d0e6d542ec5
Mikołaj <mikolaj.walkowski.00@gmail.com>
2022-01-23 13:48:49 +0100

    TempFixing CI

--- JSON_tools_client/src/main/java/module-info.java
+++ JSON_tools_client/src/main/java/module-info.java
@@ -1,4 +1,8 @@
-/**
- * Main package
- * 
- */
+module com.example.json_tools_client {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.json_tools_client to javafx.fxml;
+    exports com.example.json_tools_client;
+}
